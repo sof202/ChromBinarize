84520e255e00101c630de157b78d0ef128d15df3
sof202 <s.o.fletcher@exeter.ac.uk>
2024-06-03 14:03:15 +0100

    allow N or % to be used

--- erroneous_rate_plot.sh
+++ erroneous_rate_plot.sh
@@ -23,19 +23,29 @@ mv "${SLURM_SUBMIT_DIR}/pvalues${SLURM_JOB_ID}.err" \
   "${SCRIPT_DIR}/logs/pvalues${SLURM_JOB_ID}.err"
 
 bed_file=$1
-mark=$2
-max_read_depth=$3
-methylation_threshold=$4
+mark=$2 # m for methylation, h for hydroxymethylation
+threshold=$3
+max_read_depth=$4
+run_type=$5 # N for read_number, otherwise looks at percentage
 
 mkdir -p "$SCRIPT_DIR/plots/"
 
 module purge
 module load R
 
-Rscript "$SCRIPT_DIR/erroneous_rate_plot.R" \
-  "$bed_file" \
-  "$mark" \
-  "$max_read_depth" \
-  "$methylation_threshold" \
-  "$SCRIPT_DIR/plots/unmethylation_${mark}_${methylation_threshold}.png" \
-  "$SCRIPT_DIR/plots/methylation_${mark}_${methylation_threshold}.png"
+if [ "$run_type" == "N" ]; then
+  Rscript "$SCRIPT_DIR/erroneous_rate_plot_N.R" \
+    "$bed_file" \
+    "$mark" \
+    "$max_read_depth" \
+    "$threshold" \
+    "$SCRIPT_DIR/plots/unmethylation_${mark}_percent_${threshold}.png" \
+    "$SCRIPT_DIR/plots/methylation_${mark}_percent_${threshold}.png"
+else
+  Rscript "$SCRIPT_DIR/erroneous_rate_plot_percent.R" \
+    "$bed_file" \
+    "$mark" \
+    "$threshold" \
+    "$SCRIPT_DIR/plots/unmethylation_${mark}_N_${threshold}.png" \
+    "$SCRIPT_DIR/plots/methylation_${mark}_N_${threshold}.png"
+fi
