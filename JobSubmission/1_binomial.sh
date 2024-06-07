f63d07d38b2c730d9919aa8f8ac8d8a2c67c062f
sof202 <s.o.fletcher@exeter.ac.uk>
2024-06-07 13:17:24 +0100

    update options for script

--- JobSubmission/1_binomial.sh
+++ JobSubmission/1_binomial.sh
@@ -38,6 +38,12 @@ Dependencies: R, awk
 Inputs:
 \$1 -> base folder (for data)
 \$2 -> bed file location
+\$3 -> minimum read depth to consider in subsequent analysis
+\$4 -> min read depth for methylation reference dataset
+\$5 -> min percent_methylation for methylation reference dataset
+\$6 -> min read depth for hydroxymethylation reference dataset
+\$7 -> min percent_methylation for hydroxymethylation reference dataset
+\$8 -> flag for removing unmethylated sites
 ================================================================================
 EOF
     exit 0
@@ -51,17 +57,57 @@ if [ -z "$2" ]; then usage; fi
 
 base_folder=$1
 bed_file_location=$2
+minimum_read_depth=${3:-30}
+
+reference_read_depth_threshold_m=${4:-500}
+reference_percentage_threshold_m=${5:-95}
+reference_read_depth_threshold_h=${6:-50}
+reference_percentage_threshold_h=${7:-95}
+
+remove_unmethylated_sites=$8
 
 rm -rf "${base_folder}/5mc" "${base_folder}5hmc"
 mkdir -p "${base_folder}/5mc" "${base_folder}5hmc"
 
-awk '$4 == "m" && $5 >= 500 && $7 >= 95 {print $5","$7}' "${bed_file_location}" > "${base_folder}/5mc/methylated.csv"
-awk '$4 == "m" && $5 >= 500 && $7 <= 5 {print $5","$7}' "${bed_file_location}" > "${base_folder}/5mc/unmethylated.csv"
-awk '$4 == "m" && $5 >= 30' "${bed_file_location}" > "${base_folder}/5mc/filtered_reads.bed"
+## ================ ##
+##  5mC filtering   ##
+## ================ ##
 
-awk '$4 == "h" && $5 >= 50 && $7 >= 95 {print $5","$7}' "${bed_file_location}" > "${base_folder}/5hmc/methylated.csv"
-awk '$4 == "h" && $5 >= 50 && $7 <= 5 {print $5","$7}' "${bed_file_location}" > "${base_folder}/5hmc/unmethylated.csv"
-awk '$4 == "h" && $5 >= 30' "${bed_file_location}" > "${base_folder}/5hmc/filtered_reads.bed"
+awk -v percent_threshold="${reference_percentage_threshold_m}" \
+  -v read_threshold="${reference_read_depth_threshold_m}" \
+  '$4 == "m" && $5 >= read_threshold && $7 >= percent_threshold {print $5","$7}' \
+  "${bed_file_location}" > "${base_folder}/5mc/methylated.csv"
+
+awk -v percent_threshold=$((100 - "${reference_percentage_threshold_m}")) \
+  -v read_threshold="${reference_read_depth_threshold_m}" \
+  '$4 == "m" && $5 >= read_threshold && $7 <= 5 {print $5","$7}' \
+  "${bed_file_location}" > "${base_folder}/5mc/unmethylated.csv"
+
+awk -v read_threshold="${minimum_read_depth}" \
+  '$4 == "m" && $5 >= read_threshold' \
+  "${bed_file_location}" > "${base_folder}/5mc/filtered_reads.bed"
+
+## ================= ##
+##  5hmC filtering   ##
+## ================= ##
+#
+awk -v percent_threshold="${reference_percentage_threshold_h}" \
+  -v read_threshold="${reference_read_depth_threshold_h}" \
+  '$4 == "m" && $5 >= read_threshold && $7 >= percent_threshold {print $5","$7}' \
+  "${bed_file_location}" > "${base_folder}/5hmc/methylated.csv"
+
+awk -v percent_threshold=$((100 - "${reference_percentage_threshold_h}")) \
+  -v read_threshold="${reference_read_depth_threshold_h}" \
+  '$4 == "m" && $5 >= read_threshold && $7 <= 5 {print $5","$7}' \
+  "${bed_file_location}" > "${base_folder}/5hmc/unmethylated.csv"
+
+awk -v read_threshold="${minimum_read_depth}" \
+  '$4 == "m" && $5 >= read_threshold' \
+  "${bed_file_location}" > "${base_folder}/5hmc/filtered_reads.bed"
+
+## ========================= ##
+##   RUN BINOMIAL ANALYSIS   ##
+## ========================= ##
 
 module purge
 module load R/4.2.1-foss-2022a
@@ -71,5 +117,16 @@ Rscript "${RSCRIPT_DIR}/binom.R" "${base_folder}/5hmc"
 
 module purge
 
-awk '$8 > 1.7e-8 && $9 < 1.7e-8' "${base_folder}/5mc/processed_reads.bed" > "${base_folder}/5mc/purified_reads.bed"
-awk '$8 > 1.7e-8 && $9 < 1.7e-8' "${base_folder}/5hmc/processed_reads.bed" > "${base_folder}/5hmc/purified_reads.bed"
+## ============================= ##
+##   REMOVE UNMETHYLATED SITES   ##
+## ============================= ##
+
+if [ -z "${remove_unmethylated_sites}" ]; then
+  awk '$8 > 1.7e-8 && $9 < 1.7e-8' \
+    "${base_folder}/5mc/processed_reads.bed" > \
+    "${base_folder}/5mc/purified_reads.bed"
+
+  awk '$8 > 1.7e-8 && $9 < 1.7e-8' \
+    "${base_folder}/5hmc/processed_reads.bed" > \
+    "${base_folder}/5hmc/purified_reads.bed"
+fi
