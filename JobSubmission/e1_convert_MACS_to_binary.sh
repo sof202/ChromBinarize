8e9e1ab59d6806254c6d910e80bb0d8aa2fb3c5d
sof202 <s.o.fletcher@exeter.ac.uk>
2024-06-26 12:54:48 +0100

    add script to convert MACS peak calling bed files into ChromHMM binary files

---
+++ JobSubmission/e1_convert_MACS_to_binary.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#SBATCH --export=ALL
+#SBATCH -p mrcq 
+#SBATCH --time=03:00:00 
+#SBATCH -A Research_Project-MRC190311 
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem=4G 
+#SBATCH --mail-type=END 
+#SBATCH --output=convert%j.log
+#SBATCH --error=convert%j.err
+#SBATCH --job-name=convert
+
+SCRIPT_PATH=$(scontrol show job "$SLURM_JOBID" | \
+  awk '/Command=/{print $1}' | \
+  cut -d= -f1)
+SCRIPT_DIR=$(realpath "$(dirname "$SCRIPT_PATH")")
+
+ROOT_DIR="${SCRIPT_DIR}/.."
+RSCRIPT_DIR="${ROOT_DIR}/Rscripts"
+
+mkdir -p "${ROOT_DIR}/logs/"
+mv "${SLURM_SUBMIT_DIR}/compare${SLURM_JOB_ID}.log" \
+  "${ROOT_DIR}/logs/compare${SLURM_JOB_ID}.log"
+mv "${SLURM_SUBMIT_DIR}/compare${SLURM_JOB_ID}.err" \
+  "${ROOT_DIR}/logs/compare${SLURM_JOB_ID}.err"
+
+usage() {
+cat <<EOF
+================================================================================
+e1_convert_MACS_to_binary.sh
+================================================================================
+Purpose: Converts narrow peak or broad peaks to binary format for chromHMM
+Author: Sam Fletcher
+Contact: s.o.fletcher@exeter.ac.uk
+Dependencies: R, awk, bedtools
+================================================================================
+EOF
+    exit 0
+}
+
+if [ "$#" -eq 0 ]; then usage; fi 
+
+## ======== ##
+##   MAIN   ##
+## ======== ##
+
+# config will source all of the variables seen below
+config_file_location=$1
+source "${config_file_location}" || exit 1
+
+module purge
+module load R/4.2.1-foss-2022a
+
+mkdir -p "${base_folder}/4_BinarizedFiles"
+
+Rscript ${RSCRIPT_DIR}/create_blank_bed_files.R \
+  "${chromosome_sizes}" \
+  "${bin_size}" \
+  "${base_folder}/4_BinarizedFiles/${epigenetic_mark}"
+
+module purge
+module load BEDTools
+
+mkdir -p "${base_folder}/4_BinarizedFiles/${epigenetic_mark}"
+
+for chr in {1..22} X; do
+  bedtools intersect \
+    -wa \
+    -c \
+    -a "${base_folder}/4_BinarizedFiles/${epigenetic_mark}/chromosome${chr}.bed" \
+    -b "${input_MACS_file}" | \
+    awk '{OFS="\t"} {print ($4 > 0 ? 1 : 0)}' | \
+    gzip > \
+    "${base_folder}/4_BinarizedFiles/${epigenetic_mark}/${cell_type}_chr${chr}_binary.txt.gz"
+
+  rm "${base_folder}/4_BinarizedFiles/${epigenetic_mark}/chromosome${chr}.bed"
+done
+
+
+
+
