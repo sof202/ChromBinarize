0b088baad5867e956b0faf149d2f9980a4acc2c5
sof202 <s.o.fletcher@exeter.ac.uk>
2024-06-27 10:58:56 +0100

    fix: previously wasn't using variables but hard coded values

--- JobSubmission/1_binomial.sh
+++ JobSubmission/1_binomial.sh
@@ -64,7 +64,7 @@ awk -v percent_threshold="${reference_percentage_threshold_m}" \
 
 awk -v percent_threshold=$((100 - ${reference_percentage_threshold_m})) \
   -v read_threshold="${reference_read_depth_threshold_m}" \
-  '$4 == "m" && $5 >= read_threshold && $7 <= 5 {print $5","$7}' \
+  '$4 == "m" && $5 >= read_threshold && $7 <= percent_threshold {print $5","$7}' \
   "${bed_file_location}" > "${base_folder}/5mc/unmethylated.csv"
 
 awk -v read_threshold="${minimum_read_depth}" \
@@ -82,7 +82,7 @@ awk -v percent_threshold="${reference_percentage_threshold_h}" \
 
 awk -v percent_threshold=$((100 - ${reference_percentage_threshold_h})) \
   -v read_threshold="${reference_read_depth_threshold_h}" \
-  '$4 == "h" && $5 >= read_threshold && $7 <= 5 {print $5","$7}' \
+  '$4 == "h" && $5 >= read_threshold && $7 <= percent_threshold {print $5","$7}' \
   "${bed_file_location}" > "${base_folder}/5hmc/unmethylated.csv"
 
 awk -v read_threshold="${minimum_read_depth}" \
