de1ddeb4bcf0937a2cff114170b4065b6a2bdfa9
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-03 10:44:44 +0100

    fix indentation

--- functions/intersect.sh
+++ functions/intersect.sh
@@ -12,13 +12,13 @@ intersect_intersectBSWithONT() {
     -wo \
     -a "${ONT_bed_file}" \
     -b "${BS_bed_file}" | \
-      awk \
-      'function convert_to_percent(reads, total_reads) {
-         return (int(reads / total_reads * 10000) / 100)
-      }
-      {OFS="\t"} 
-      {print $1,$2,$3,$4,$5,$7,$12,convert_to_percent($11,$12)}' > \
+    awk \
+    'function convert_to_percent(reads, total_reads) {
+      return (int(reads / total_reads * 10000) / 100)
+    }
+    {OFS="\t"} 
+    {print $1,$2,$3,$4,$5,$7,$12,convert_to_percent($11,$12)}' > \
       "${output_file_path}"
 
-  module purge
+    module purge
 }
