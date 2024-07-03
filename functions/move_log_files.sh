d2dd35637ee625ccce5e488e96a4cbf94a3e5837
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-03 10:44:13 +0100

    update function and indentation

--- functions/move_log_files.sh
+++ functions/move_log_files.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 move_log_files() {
-    # Moves log files from job submission directory to designated output
-    # directory.
-    original_name=$1
+   # SLURM workload manager is incapable of creating directories or using 
+   # variables when generating log/error files. As a result, scripts create
+   # temporary log/error files and this function moves them instead.
+   original_name=$1
 
-    mkdir -p "${LOG_DIR}/"
-    mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.log" \
-       "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.log"
-    mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.err" \
-       "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.err"
+   mkdir -p "${LOG_DIR}/"
+   mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.log" \
+      "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.log"
+         mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.err" \
+            "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.err"
 }
