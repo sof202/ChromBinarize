4c4d6c6775c8bfeea63a37cca65f47a4085a6375
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-03 09:40:25 +0100

    update script to work with BS-Seq data as well

--- JobSubmission/supplementary/a_erroneous_rate_plot.sh
+++ JobSubmission/supplementary/a_erroneous_rate_plot.sh
@@ -28,11 +28,6 @@ EOF
 
 if [ "$#" -eq 0 ]; then usage; fi 
 
-## ======== ##
-##   MAIN   ##
-## ======== ##
-
-# config will source all of the variables seen below
 config_file_location=$1
 source "${config_file_location}" || exit 1
 
@@ -41,22 +36,40 @@ source "${REPO_DIR}/parameters.txt" || exit 1
 source "${FUNCTIONS_DIR}/move_log_files.sh" || exit 1
 move_log_files pvalues
 
+number_of_columns=$(awk '{print NF; exit}' "${bed_file_location}")
+
+if [[ "${number_of_columns}" -eq 5 ]]; then
+    source "${FUNCTIONS_DIR}/purification.sh" || exit 1
+
+    purification_convertBSBedToMethylBedFormat "${BASE_DIR}/converted.bed" "${bed_file_location}" "${mark}"
+
+    bed_file_location="${BASE_DIR}/converted.bed"
+fi
+
+## ======== ##
+##   MAIN   ##
+## ======== ##
+
 mkdir -p "${BASE_DIR}/plots/"
 
 module purge
 module load R/4.2.1-foss-2022a
 
-if [ "$run_type" == "N" ]; then
-  Rscript "$RSCRIPT_DIR/erroneous_rate_plot_N.R" \
-    "$bed_file_location" \
-    "$mark" \
-    "$max_N_value" \
-    "$plot_type" \
+if [[ "${run_type}" == "N" ]]; then
+  Rscript "${RSCRIPT_DIR}/erroneous_rate_plot_N.R" \
+    "${bed_file_location}" \
+    "${mark}" \
+    "${max_N_value}" \
+    "${plot_type}" \
     "${BASE_DIR}/plots/erroneous_rate_plot_${plot_type}_${run_type}_x_axis.png"
-else
-  Rscript "$RSCRIPT_DIR/erroneous_rate_plot_percent.R" \
-    "$bed_file_location" \
-    "$mark" \
-    "$plot_type" \
+elif [[ "${run_type}" == "percent" ]]; then
+  Rscript "${RSCRIPT_DIR}/erroneous_rate_plot_percent.R" \
+    "${bed_file_location}" \
+    "${mark}" \
+    "${plot_type}" \
     "${BASE_DIR}/plots/erroneous_rate_plot_${plot_type}_${run_type}_x_axis.png"
+fi
+
+if [[ "${number_of_columns}" -eq 5 ]]; then
+    rm "${BASE_DIR}/converted.bed" 
 fi
