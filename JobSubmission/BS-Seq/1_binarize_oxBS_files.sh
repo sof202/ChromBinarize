0a035b1c74661020bd2504db5fddc315908b94a5
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-16 11:24:59 +0100

    fix: still refering to hydroxymethylation in places

--- JobSubmission/BS-Seq/1_binarize_oxBS_files.sh
+++ JobSubmission/BS-Seq/1_binarize_oxBS_files.sh
@@ -7,19 +7,19 @@
 #SBATCH --ntasks-per-node=16
 #SBATCH --mem=4G 
 #SBATCH --mail-type=END 
-#SBATCH --output=hydroxy%j.log
-#SBATCH --error=hydroxy%j.err
-#SBATCH --job-name=hydroxy
+#SBATCH --output=oxBS_5mc%j.log
+#SBATCH --error=oxBS_5mc%j.err
+#SBATCH --job-name=oxBS_5mc
 
 usage() {
 cat <<EOF
 ================================================================================
 1_binarize_oxBS_files.sh
 ================================================================================
-Purpose: Create binary files for dense and sparse regions of pure methylation 
-from oxidative bisulphite sequencing bed files. Bed files are expected to be 
-in the format:
-  chr \t start \t end \t hydroxymethylated_reads \t total_reads  
+Purpose: Create binary files for dense and sparse regions of 5mC from 
+oxidative bisulphite sequencing bed files. Bed files are expected to be in the 
+format:
+  chr \t start \t end \t 5mC_reads \t total_reads  
 Author: Sam Fletcher
 Contact: s.o.fletcher@exeter.ac.uk
 Dependencies: R, awk, bedtools
@@ -36,7 +36,7 @@ source "${config_file_location}" || { usage; exit 1; }
 source "${REPO_DIR}/parameters.txt" || exit 1
 
 source "${FUNCTIONS_DIR}/move_log_files.sh" || exit 1
-move_log_files hydroxy
+move_log_files oxBS_5mc
 
 processing_directory="${BASE_DIR}/oxBS_5mc"
 
@@ -48,10 +48,16 @@ rm -rf "${processing_directory}"
 mkdir -p "${processing_directory}"
 source "${FUNCTIONS_DIR}/purification.sh" || exit 1
 
-purification_convertBSBedToMethylBedFormat "${processing_directory}/formatted.bed" "${oxBS_bed_file_location}" "h"
+purification_convertBSBedToMethylBedFormat "${processing_directory}/formatted.bed" \
+  "${oxBS_bed_file_location}" \
+  "m"
 
-purification_extractSitesWithHighMethylation "${processing_directory}" "${processing_directory}/formatted.bed" "h"
-purification_extractSitesWithLowMethylation "${processing_directory}" "${processing_directory}/formatted.bed" "h"
+purification_extractSitesWithHighMethylation "${processing_directory}" \
+  "${processing_directory}/formatted.bed" \
+  "m"
+purification_extractSitesWithLowMethylation "${processing_directory}" \
+  "${processing_directory}/formatted.bed" \
+  "m"
 purification_filterOutLowReadDepthSites "${processing_directory}" "${processing_directory}/formatted.bed" "h"
 purification_calculateSiteMethylationProbability "${processing_directory}" 
 purification_removeDeterminedUnmethylatedSites "${processing_directory}"
@@ -66,7 +72,9 @@ binarization_createDirectories "${processing_directory}"
 binarization_splitIntoChromosomes "${processing_directory}"
 binarization_createBlankBins "${processing_directory}"
 binarization_countSignalIntersectionWithBins "${processing_directory}"
-binarization_createChromhmmBinaryFiles "${processing_directory}" "${BINARY_DIR}/oxBS_5mC" "oxBS_5mC"
+binarization_createChromhmmBinaryFiles "${processing_directory}" \
+  "${BINARY_DIR}/oxBS_5mC" \
+  "oxBS_5mC"
 
 if [[ ! "${debug_mode:='false'}" == "true" ]]; then
   rm -rf "${processing_directory}"
