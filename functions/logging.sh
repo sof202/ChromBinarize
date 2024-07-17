ad02def6dbc42e570baaf51b3b684c8401ce651c
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-17 11:20:33 +0100

    feat: add logging and error functions

---
+++ functions/logging.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+logs() {
+  # allows for some messages to be logged only if in debug mode
+  is_logged=$1
+  message=$2
+
+  current_time=$(date -u +'%-d-%m ~ %T')
+
+  if [[ is_logged -eq 1 ]]; then
+cat << LOG_MESSAGE
+[LOG: ${current_time}]
+"${message}"
+LOG_MESSAGE
+  fi
+}
+
+errors() {
+  message=$1
+  current_time=$(date -u +'%-d-%m ~ %T')
+
+cat 1>&2 << ERROR_MESSAGE
+[ERROR: ${current_time}]
+"${message}"
+ERROR_MESSAGE
+}
