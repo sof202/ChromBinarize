5b7cf2321c2db85acd1796281b6e214b9145484a
sof202 <s.o.fletcher@exeter.ac.uk>
2024-07-31 14:32:46 +0100

    feat: change log file locations
    Now log files are sorted by job name and user
    
    fix: move log files to correct place

--- functions/move_log_files.sh
+++ functions/move_log_files.sh
@@ -5,10 +5,11 @@ move_log_files() {
    # variables when generating log/error files. As a result, scripts create
    # temporary log/error files and this function moves them instead.
    original_name=$1
+   datetime=$(date +%d-%h~%H-%M)
 
-   mkdir -p "${LOG_DIR}/"
+   mkdir -p "${LOG_DIR}/${USER}/${original_name}"
    mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.log" \
-      "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.log"
+      "${LOG_DIR}/${USER}/${original_name}/${datetime}.log"
    mv "${SLURM_SUBMIT_DIR}/${original_name}${SLURM_JOB_ID}.err" \
-      "${LOG_DIR}/${original_name}${SLURM_JOB_ID}.err"
+      "${LOG_DIR}/${USER}/${original_name}/${datetime}.err"
 }
