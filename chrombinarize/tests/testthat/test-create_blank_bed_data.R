052e2871b70abc486054050bd415b43a8ac0b9bd
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-13 15:32:05 +0100

    refactor: use a much smaller Rds file to test functionality

--- chrombinarize/tests/testthat/test-create_blank_bed_data.R
+++ chrombinarize/tests/testthat/test-create_blank_bed_data.R
@@ -1,18 +1,27 @@
 # Test data is a subset of hg19.txt that contains chromosomes 1-22, X and Y
 chromosome_sizes_test_file <-
   testthat::test_path("test_data", "example_chromsizes.txt")
-expected_bed_data <-
+
+# This test bed data only includes chromosome 1 as it allows for a smaller
+# rds file
+expected_small_bed_data <-
   readRDS(testthat::test_path("test_data", "expected_bed_data.Rds"))
 
 testthat::test_that("Function returns correct output for valid input", {
   bed_data <- create_blank_bed_data(
     chromosome_sizes_test_file,
     200,
-    c(seq(1, 22), "X")
+    c(seq(1, 22), "X", "Y")
+  )
+
+  small_bed_data <- create_blank_bed_data(
+    chromosome_sizes_test_file,
+    200,
+    1
   )
 
   expect_true(is.list(bed_data))
-  expect_equal(names(bed_data), c(seq(1, 22), "X"))
+  expect_equal(names(bed_data), c(seq(1, 22), "X", "Y"))
   expect_true(all(vapply(bed_data, inherits, logical(1), "data.table")))
-  expect_equal(bed_data, expected_bed_data)
+  expect_equal(small_bed_data, expected_small_bed_data)
 })
