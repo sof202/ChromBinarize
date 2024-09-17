9c7087fa258fc42ac0d5d92f62a021f0b333c3a3
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-17 11:47:01 +0100

    refactor: use new read_comparison_bedmethyl function

--- Rscripts/WGBS_comparison.R
+++ Rscripts/WGBS_comparison.R
@@ -9,19 +9,9 @@ library(ggplot2)
 ## ===================== ##
 ##   DATA MANIPULATION   ##
 ## ===================== ##
-methylation_data <- data.table::fread(input_bed_file)
-colnames(methylation_data) <- c(
-  "chr",
-  "start",
-  "end",
-  "mark",
-  "ONT_N",
-  "ONT_percent_methylation",
-  "WGBS_N",
-  "WGBS_percent_methylation"
-)
+methylation_data <- chrombinarize::read_comparison_bedmethyl(input_bed_file)
 
-# Here we combine 5mC and 5hmC as WGBS captures both signals without separating
+# Here we combine 5mC and 5hmC as BS captures both signals without separating
 # them
 methylation_data <- methylation_data |>
   dplyr::mutate(
@@ -40,13 +30,13 @@ methylation_data <- methylation_data |>
 methylation_data <- methylation_data |>
   dplyr::mutate(
     "absolute_change_percent_methylation" = abs(
-      ONT_percent_methylation - WGBS_percent_methylation
+      ONT_percent_methylation - BS_percent_methylation
     ),
-    "absolute_change_read_depth" = abs(ONT_N - WGBS_N)
+    "absolute_change_read_depth" = abs(ONT_N - BS_N)
   ) |>
   dplyr::filter(
     ONT_N >= 30, # Filtering by read depth should give better results
-    WGBS_N >= 30,
+    BS_N >= 30,
     mark == "m" # still have hydroxy rows at this point.
   )
 
@@ -77,11 +67,13 @@ breaks <- c(1, 10, 100, 1000, 10000)
 methylation_correlation_plot <-
   ggplot(
     methylation_data,
-    aes(x = ONT_percent_methylation, y = WGBS_percent_methylation)
+    aes(x = ONT_percent_methylation, y = BS_percent_methylation)
   ) +
   geom_bin2d(bins = 100) +
-  scale_fill_gradient(name = "count", trans = "log",
-                        breaks = breaks, labels = breaks) +
+  scale_fill_gradient(
+    name = "count", trans = "log",
+    breaks = breaks, labels = breaks
+  ) +
   theme_bw() +
   labs(x = "percent methylation in ONT", y = "percent methylation in WGBS")
 
--- Rscripts/oxBS_comparison.R
+++ Rscripts/oxBS_comparison.R
@@ -9,17 +9,7 @@ library(ggplot2)
 ## ===================== ##
 ##   DATA MANIPULATION   ##
 ## ===================== ##
-methylation_data <- data.table::fread(input_bed_file)
-colnames(methylation_data) <- c(
-  "chr",
-  "start",
-  "end",
-  "mark",
-  "ONT_N",
-  "ONT_percent_methylation",
-  "oxBS_N",
-  "oxBS_percent_methylation"
-)
+methylation_data <- chrombinarize::read_comparison_bedmethyl(input_bed_file)
 
 # Here we extract the hydroxymethylation
 methylation_data <- methylation_data |>
@@ -30,13 +20,13 @@ methylation_data <- methylation_data |>
 methylation_data <- methylation_data |>
   dplyr::mutate(
     "absolute_change_percent_methylation" = abs(
-      ONT_percent_methylation - oxBS_percent_methylation
+      ONT_percent_methylation - BS_percent_methylation
     ),
-    "absolute_change_read_depth" = abs(ONT_N - oxBS_N)
+    "absolute_change_read_depth" = abs(ONT_N - BS_N)
   ) |>
   dplyr::filter(
     ONT_N >= 30, # Filtering by read depth should give better results
-    oxBS_N >= 30,
+    BS_N >= 30,
   )
 
 # Purely for read depth histogram (makes plot more interpretable)
@@ -66,7 +56,7 @@ breaks <- c(1, 10, 100, 1000, 10000)
 methylation_correlation_plot <-
   ggplot(
     methylation_data,
-    aes(x = ONT_percent_methylation, y = oxBS_percent_methylation)
+    aes(x = ONT_percent_methylation, y = BS_percent_methylation)
   ) +
   geom_bin2d(bins = 100) +
   scale_fill_gradient(
