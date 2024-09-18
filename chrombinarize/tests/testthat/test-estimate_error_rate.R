3625d7ca7ac1bfec296ecc383ad175f33cce22ef
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-18 15:52:21 +0100

    test: fix tests for estimating error rate

--- chrombinarize/tests/testthat/test-estimate_error_rate.R
+++ chrombinarize/tests/testthat/test-estimate_error_rate.R
@@ -1,41 +1,44 @@
-example_input_file <-
-  testthat::test_path("test_data", "example_unmethylated_reads.bed")
-expected_error_rate <-
-  readRDS(testthat::test_path("test_data", "expected_error_rate.Rds"))
-
 test_that("error rate is being calculated correctly", {
-  calculated_error_rate <- estimate_error_rate(example_input_file)
+  normal_bedmethyl <- readRDS(test_path(
+    "test_data",
+    "estimate_error_rate",
+    "normal_bedmethyl.Rds"
+  ))
+  actual_error_rate <- estimate_error_rate(normal_bedmethyl, 30, 5)
+  expected_error_rate <- readRDS(test_path(
+    "test_data",
+    "estimate_error_rate",
+    "normal_bedmethyl_error_rate.Rds"
+  ))
 
-  expect_true(is.numeric(calculated_error_rate))
-  expect_equal(calculated_error_rate, expected_error_rate)
+  expect_true(is.numeric(actual_error_rate))
+  expect_equal(actual_error_rate, expected_error_rate)
 })
 
-
-test_that("error rate is zero for all unmethylated reads", {
-  tempfile_path <- withr::local_tempfile(
-    pattern = "allZeros.bed",
-    lines = c("10\t0", "20\t0")
-  )
-  calculated_error_rate <- estimate_error_rate(tempfile_path)
-  expect_equal(calculated_error_rate, 0)
-})
-test_that("error rate is one for all methylated reads", {
-  tempfile_path <- withr::local_tempfile(
-    pattern = "allZeros.bed",
-    lines = c("10\t100", "20\t100")
-  )
-  calculated_error_rate <- estimate_error_rate(tempfile_path)
-  expect_equal(calculated_error_rate, 1)
+test_that("error rate is zero if all reads are unmethylated", {
+  all_zeros <- readRDS(test_path(
+    "test_data",
+    "estimate_error_rate",
+    "all_zeros.Rds"
+  ))
+  actual_error_rate <- estimate_error_rate(all_zeros, 30, 5)
+  expect_equal(actual_error_rate, 0)
 })
 
-test_that("function throws error for incorrect file format", {
-  tempfile_path <- withr::local_tempfile(
-    pattern = "wrong_format.bed",
-    lines = c("string,string", "string,string")
-  )
-  expect_error(read_reference_set(tempfile_path))
-})
+test_that("error rate is NaN if no reads hit threshold", {
+  all_one_hundreds <- readRDS(test_path(
+    "test_data",
+    "estimate_error_rate",
+    "all_one_hundreds.Rds"
+  ))
+  calculated_error_rate <- estimate_error_rate(all_one_hundreds, 30, 5)
+  expect_equal(calculated_error_rate, NaN)
 
-test_that("function throws error for non-existent file", {
-  expect_error(read_reference_set("non-existent-file"))
+  all_low_read_depth <- readRDS(test_path(
+    "test_data",
+    "estimate_error_rate",
+    "all_low_read_depth.Rds"
+  ))
+  calculated_error_rate <- estimate_error_rate(all_low_read_depth, 30, 5)
+  expect_equal(calculated_error_rate, NaN)
 })
