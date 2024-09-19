833d2f36637a47ef7ab4f1bfa81f7f37dbd211d0
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-19 09:30:35 +0100

    test: add test for error rate plot data

---
+++ chrombinarize/tests/testthat/test-error_rate_plots.R
@@ -0,0 +1,14 @@
+test_that("Expected value is obtained", {
+  methylation_data <- readRDS(test_path(
+    "test_data",
+    "read_bedmethyl_files",
+    "expected_bedmethyl_data.Rds"
+  ))
+  actual_error_rate_data <- create_error_rate_data(methylation_data, 60, 5)
+  expected_error_rate_data <- readRDS(test_path(
+    "test_data",
+    "error_rate_plot_data",
+    "expected_error_rate_data.Rds"
+  ))
+  expect_equal(actual_error_rate_data, expected_error_rate_data)
+})
